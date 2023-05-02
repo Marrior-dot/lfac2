462a6fb0a23ca10ebfa3b2e8732fc89bb1ef6033
Marrior-dot <59967704+Marrior-dot@users.noreply.github.com>
2023-05-02 11:57:19 -0300

    Add files via upload

--- import re.py
+++ import re.py
@@ -4,26 +4,33 @@ import re
 
 test =  str(input("Descreva a sequência de caracteres"))
 #while alfabet not in test:
-#    test =  str(input("Descreva sequência de caracteres contendo M, H, m e h"))
+#   test =  str(input("Descreva sequência de caracteres contendo M, H, m e h"))
  
 
 
 
-testmatcha = re.match('(MH|HM)((m|h)*m+(m|h)*m+(m|h)*)$|((m|h)*h+(m|h)*)$|((m|h)*h+(m|h)*h+(m|h)*)$', test, flags=0)
-testmatchb = re.match('^(MH|HM)(mm|h)*m+(mm|h)*$',test, flags = 0)
-testmatchc = re.match('^(MH|HM)(m)(h|m)*(h)$', test, flags=0)
-testmatchd = re.match('^(MM|HH)(mm|hh|mh|hm)(mm|hh|mh|hm)(h|m)*(mm|hh|mh|hm)$', test, flags=0)
-testmatche = re.match('^(MM|HH)((m?)(hm)*(h?))$|((h?)(mh)*(m?))$', test, flags=0)
-testmatchf = re.match('^(MM|HH)(h(m|mh)*m{0,1})$|(m(m|hm)*h{0,1})$', test, flags=0)
+testmatcha = re.match(r'(MH|HM)(((m|h)*m+(m|h)*m+(m|h)*)|((m|h)*h+(m|h)*)|((m|h)*h+(m|h)*h+(m|h)*))', test, flags=0)
+testmatchb = re.match(r'(MH|HM)(mm|h)*m(mm|h)*',test, flags=0)
+testmatchc = re.match(r'(MH|HM)m(h|m)*(h)$', test, flags=0)
+testmatchd = re.match(r'(MM|HH)(mm|hh|mh|hm)(mm|hh|mh|hm)(h|m)*(mm|hh|mh|hm)$', test, flags=0)
+testmatche = re.match(r'(MM|HH)((m?)(hm)*(h?))$|((h?)(mh)*(m?))$', test, flags=0)
+testmatchf = re.match(r'(MM|HH)((h(m|mh)*m?)|(m(m|hm)*h?))', test, flags=0)
 
 
-#if testmatchb:
-#    print(True)
-#if testmatchc:
-#    print(True)
-#if testmatchd:
-#    print(True)
-#if testmatche:
-#    print(True)    
-if testmatchf:
-    print(True)
+if bool(testmatcha) == True:
+   print("caso a)")
+   
+if bool(testmatchb) == True:
+   print("caso b)")
+    
+if bool(testmatchc) == True:
+    print("caso c)")
+    
+if bool(testmatchd) == True:
+    print("caso d)")
+    
+if bool(testmatche) == True:
+    print("caso e)")
+        
+if bool(testmatchf) == True:
+    print("caso f)")
